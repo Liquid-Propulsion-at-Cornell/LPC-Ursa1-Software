09988886b9d13c5178c07a122974db3cc6965fc1
tmt115 <miketommy2006@gmail.com>
2026-03-21 15:30:56 -0400

    Update analog_script.lua

--- analog_script.lua
+++ analog_script.lua
@@ -1,12 +1,22 @@
+--[[
+    LPC LABJACK T7 LUA CONFIG SCRIPT
+    Author: Thomas Tedeschi
+    Last Updated Date: 3/21/2026
+--]]
+
 local mb = require("Modbus")
 
-local pos_pin = 0
-local neg_pin = 1
-local res_ind = 8
-local settle = 0
-local celsius = 1
-local farenheit = 2
+-- DEFINE LOCAL VARIABLES HERE --
+--local celsius = 1
+--local farenheit = 2
+-- ...
+---------------------------------
 
+ --[[ 
+    Configre one thermocouple with positive voltage ain at ppin against
+    npin, resolution rind, voltage range +-0.1V, settling time of set us, 
+    and temperature celsius or farenheit.
+ --]]
 function configure_thermocouple(ppin, npin, rind, set, temp)
     local pos_name = "AIN" .. ppin
     local neg_name = "AIN" .. ppin .. "_NEGATIVE_CH"
@@ -18,11 +28,52 @@ function configure_thermocouple(ppin, npin, rind, set, temp)
     mb.writeName(pos_name .. "_EF_CONFIG_A", temp)
 end
 
-function configure_transducer()
+--[[ 
+    Configure one pressure transducer between input ain pin
+    apin and ground, with voltage range +-range V, and resolution
+    rind. 
+    ***TRANSDUCER SHOULD GO THROUGH A RESISTOR TO TURN CURRENT TO 
+    VOLTAGE.
+--]]
+function configure_transducer(apin, range, rind)
+    local ain_name = "AIN" .. apin
+    mb.writeName(ain_name .. "_NEGATIVE_CH", 199)
+    mb.writeName(ain_name .. "_RANGE", range)
+    mb.writeName(ain_name .. "_RESOLUTION_INDEX", rind)
+    mb.writeName(ain_name .. "_SETTLING_US", 0)
+end
 
-configure_thermocouple(pos_pin, neg_pin, res_ind, settle, celsius)
+--[[
+    Uses the ain pin to measure the input voltage through
+    a known resistor and calculates the current
+--]]
+function measure_transducer_current(apin, resistance)
+    local voltage = mb.readName("AIN" .. apin)
+    local curr = voltage/resistance
+    return curr
+end
 
+--[[
+    Takes in current to the ain to calculate the pressure using a formula
+    based the min and max currents, which we know to be 4 and 20ma, and 
+    the min and max pressures, which we can change.
+--]]
+function current_to_pressure(current, pmin, pmax)
+    local pressure = ((current-0.004)/(0.016)) * (pmax-pmin) + pmin
+    return pressure
+
+
+-- Configure the pins
+configure_thermocouple(pos_pin, neg_pin, temp_res_ind, settle, celsius)
+configure_transducer(ain_pin, v_range, pres_res_ind)
+
+-- Loop to keep checking our values and print them out
 while(true)
-    local tempC = mb.readName(pos_name .. "_EF_READ_A")
-    print("Temperature (C): ", tempC)
+    local temp = mb.readName(pos_name .. "_EF_READ_A")
+    local c = measure_transducer_current(pr_apin, resistor)
+    local p = current_to_pressure(c, p_min, p_max)
+
+    -- This is assuming Celsius can change the units
+    print("Temperature (C): ", temp)
+    print("pressure (psi): ", p)
 end
